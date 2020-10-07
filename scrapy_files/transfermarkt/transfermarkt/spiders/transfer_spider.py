0e3109d0fa4e3edeafa516a7bc2181ef2d2a576c
meehirpathare <54690720+meehirpathare@users.noreply.github.com>
2020-10-07 14:35:05 -0400

    working year
    working basic attributes, no player redirect
    per year

--- scrapy_files/transfermarkt/transfermarkt/spiders/transfer_spider.py
+++ scrapy_files/transfermarkt/transfermarkt/spiders/transfer_spider.py
@@ -4,6 +4,7 @@
 # your spiders.
 import scrapy
 import json
+from scrapy.linkextractors import LinkExtractor
 
 class TransferSpider(scrapy.Spider):
     name = "transfer_prices"
@@ -14,14 +15,40 @@ class TransferSpider(scrapy.Spider):
         "HTTPCACHE_ENABLED": True
     }
 
-    start_urls = ['https://www.transfermarkt.co.uk/transfers/saisontransfers/statistik?land_id=0&ausrichtung=&spielerposition_id=&altersklasse=&leihe=&transferfenster=&saison-id=2020']
+    start_urls = ['https://www.transfermarkt.co.uk/transfers/saisontransfers/statistik?land_id=0&ausrichtung=&spielerposition_id=&altersklasse=&leihe=&transferfenster=&saison-id=2020&plus=1']
+    
 
     def parse(self, response):
-        for i in range(1, 26):
+        for i in range(1, 50):
             path = "/html/body/div[2]/div[8]/div[1]/div/div[3]/div/table/tbody/tr[{}]//text()".format(i)
-        # player_name = response.xpath("/html/body/div[2]/div[8]/div[1]/div/div[3]/div/table/tbody/tr[1]//text()")[9].get()    
-            #player_name = response.xpath(path)[9].get()
-            yield {
-                'player_name' :  response.xpath(path)[9].get()
-                }
+            #player_name = response.xpath("/html/body/div[2]/div[8]/div[1]/div/div[3]/div/table/tbody/tr[1]//text()")[9].get()
+            #player_str = "/html/body/div[2]/div[8]/div[1]/div/div[3]/div/table/tbody/tr[1]/td[2]/table/tbody/tr[{}]/td[2]/a".format(i)
+            # player_url = response("/html/body/div[2]/div[8]/div[1]/div/div[3]/div/table/tbody/tr[1]/td[2]/table/tbody/tr[1]/td[2]/a").get()
+            try:
+                yield { 
+                    'player_name' : response.xpath(path)[9].extract(),
+                    'position' : response.xpath(path)[14].extract(),
+                    'age' : response.xpath(path)[18].extract(),
+                    'transfermarkt_val' : response.xpath(path)[19].extract(),
+                    'team_left' : response.xpath(path)[26].extract(),
+                    'league_left' : response.xpath(path)[33].extract(),
+                    'joined_team' : response.xpath(path)[44].extract(),
+                    'joined_league': response.xpath(path)[51].extract(),
+                    'fee': response.xpath(path)[56].extract()
+                    
+                    }
+            except:
+                pass
+        next_page = response.xpath("/html/body/div[2]/div[8]/div[1]/div/div[3]/div/div[2]/ul/li[13]/a//@href").get()
+        #next_page = response.xpath("/html/head/link[42]//@href")[0].extract()
+        # next_page= response.xpath("/html/head/link[42]").get()
+        url_str = ''.join(map(str, next_page))     #coverts list to str
         
+        #yield response.follow(next_page, self.parse)
+        
+        
+
+        #yield response.follow(next_page, self.parse)
+       #response.xpath("/html/head/link[42]")
+       # cd  
+       # response.xpath("/html/body/div[2]/div[8]/div[1]/div/div[3]/div/div[2]/ul/li[13]/a")
